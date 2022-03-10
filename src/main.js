29dd667f6c2b3007cbc32a0e28f405d680c99093
Victor <victor22158403@hotmail.com>
2022-03-09 23:20:46 -0300

    Comentário

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue'
+import App from './App'
 
-createApp(App).mount('#app')
+new Vue({
+    render: h => h(App) // Versão do Client.
+    /*
+    O 'h' seria o 'createElement'
+
+    render(createElement) {
+        return createElement(App)
+    }
+    */
+}).$mount("#app") // Versão do Client. O $mount("#app") tem o mesmo objetivo q o el: '#app'. Este id aponta para o q esta no 'index'
